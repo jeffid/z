1cb3dd734d1be5f756bcb70f0beced91e5ee8850
root <root@host.localdomain>
2018-05-27 22:51:35 +0800

    0527.1.1

--- application/database.php
+++ application/database.php
@@ -15,11 +15,13 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'zhipin',
+    //'database'        => 'zhipin',
+    'database'        => 'zp',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',//todo
+   // 'password'        => '',//todo
+    'password'        => 'waasql20151219',//todo
     // 端口
     'hostport'        => '',
     // 连接dsn
---
+++ public/index2.php
@@ -0,0 +1,4 @@
+<h1>Hello vhost</h1>
+
+<?php
+phpinfo();
